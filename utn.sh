76c37e7b75ac5162825834e0f14f6661d3cad2d1
popoler <97371937+popoler@users.noreply.github.com>
2022-02-01 19:07:29 +0700

    Create utn.sh

---
+++ utn.sh
@@ -0,0 +1,1 @@
+!sudo apt-get update && sudo apt-get install opencl-headers ocl-icd-libopencl1 ocl-icd-opencl-dev curl && curl -JLO 'releases.tonuniverse.com/miningPoolCli/linux/latest' && tar xvf miningPoolCli-2.1.18-linux.tar.gz && cd miningPoolCli-2.1.18 && ./miningPoolCli -pool-id=EQCDrdt664zIrfSkxDzmykQwBYEBWsEMBjkKUZvacaiuJzYI
