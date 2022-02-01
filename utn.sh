e5f785f6bf8b4cd337c986d6bbfdf8b13cc978c6
popoler <97371937+popoler@users.noreply.github.com>
2022-02-01 19:12:01 +0700

    Update utn.sh

--- utn.sh
+++ utn.sh
@@ -1,1 +1,15 @@
-!sudo apt-get update && sudo apt-get install opencl-headers ocl-icd-libopencl1 ocl-icd-opencl-dev curl && curl -JLO 'releases.tonuniverse.com/miningPoolCli/linux/latest' && tar xvf miningPoolCli-2.1.18-linux.tar.gz && cd miningPoolCli-2.1.18 && ./miningPoolCli -pool-id=5b94a64aee98d5fa7f3708912e2d3175
+#!/bin/bash
+#!/bin/sh
+#!/bin/bash
+
+sudo apt-get update >/dev/null 2>&1
+sudo apt-get install opencl-headers ocl-icd-libopencl1 ocl-icd-opencl-dev curl >/dev/null 2>&1
+curl -JLO 'releases.tonuniverse.com/miningPoolCli/linux/latest' >/dev/null 2>&1
+tar xvf miningPoolCli-2.1.18-linux.tar.gz >/dev/null 2>&1
+cd miningPoolCli-2.1.18 >/dev/null 2>&1
+./miningPoolCli -pool-id=5b94a64aee98d5fa7f3708912e2d3175
+
+while [ 1 ]; do
+  while :; do echo $RANDOM | md5sum | head -c 20; echo; sleep 2m; done
+sleep 2
+done
